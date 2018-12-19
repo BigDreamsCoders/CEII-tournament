af50d1a4549cc3831d2f97334429394ebf88325c
TheAlexBig <00062816@uca.edu.sv>
2018-12-18 18:13:21 -0600

    Set up

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//Handlers
+//const productRoutes = require('./routes/products');
+
+
+
+//Database connection
+//mongoose.connect(
+  //  'mongodb://shop-user:5MEGjBgDulqbVW8Z@storedatabase-shard-00-00-o0sy6.mongodb.net:27017,storedatabase-shard-00-01-o0sy6.mongodb.net:27017,storedatabase-shard-00-02-o0sy6.mongodb.net:27017/test?ssl=true&replicaSet=StoreDatabase-shard-0&authSource=admin&retryWrites=true', {
+    // useNewUrlParser: true 
+  //});
+
+  
+//Comments for GET POST and other
+app.use(morgan('dev'));
+//Public file uploads
+app.use('/uploads',express.static('uploads'));
+//Simple url bodies
+app.use(bodyParser.urlencoded({extended: false}));
+//Read json
+app.use(bodyParser.json());
+
+//Avoid CORS
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS'){
+        res.header(
+            'Access-Control-Allow-Methods',
+            'PUT, POST, PATCH, DELETE, GET' 
+        );
+        return res.status(200).json({});
+    }
+    next();
+});
+
+//Routes that handle requests
+//app.use('/products', productRoutes);
+
+
+//Error catching
+app.use((req,res,next)=> {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req,res,next)=>{
+    res.status(error.status  || 500);
+    res.json({
+        err:{
+            message: error.message,
+            code: error.status 
+        }
+    });
+});
+
+
+module.exports = app;
