b48c0d1dd7ccbdaa8fbd43f162e587c300dbdb35
TheAlexBig <00062816@uca.edu.sv>
2018-12-18 18:48:05 -0600

    Tweak in user

--- models/usuario.js
+++ models/usuario.js
@@ -4,7 +4,13 @@ var tournamentSchema = mongoose.Schema({
     nombre: {type: String, require:true},
     apellido: {type: String, require:true},
     identificador: {type: Map, of: String, require:true},
-    imagenPerfil: {type: String, require: false}
+    imagenPerfil: {type: String, require: false},
+    secreto: {type:String},
+    correo: {
+        type: String, 
+        require: true, 
+        match: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/},
+    rol: {type:String, deafult:"comun"}
 });
 
 module.exports = mongoose.model('Usuario',tournamentSchema);
