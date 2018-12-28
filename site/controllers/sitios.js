ebbedacb3f15497020089332b5c72633dc268aac
TheAlexBig <00062816@uca.edu.sv>
2018-12-27 20:11:33 -0600

    Se muestra infomacion en la app web

--- site/controllers/sitios.js
+++ site/controllers/sitios.js
@@ -1,3 +1,9 @@
+const Torneo = require('../../API/models/torneo');
+const Participante = require('../../API/models/participante');
+
+const estandar = require('../../API/tools/estandar');
+
+
 exports.render_index =  (req, res, next) => {
     res.status(200).render('index', { 
         title: 'Hey', 
@@ -7,9 +13,41 @@ exports.render_index =  (req, res, next) => {
 };
 
 exports.render_torneos =  (req, res, next) => {
-    res.status(200).render('torneos', { 
-    title: 'Torneo', 
-    mensaje: 'Torneos here!', 
-    texto:'Torneos aqui',
-    parrafo:'Parrafo de torneos'});
+    Torneo.find({}, null,{sort: {nombre: 1}}, (err, data)=>{
+        if(err){
+            estandar.errorChecker(res,err);
+        }
+    })
+    .select('_id nombre facultad creadoEl fechaEvento lugarEvento precioEntrada cupoEvento imagenTorneo')
+    .exec()
+    .then(docs => {
+        res.status(200).render('torneos', { 
+            title: 'Torneos', 
+            mensaje: 'Los torneos se desplegaran', 
+            count: docs.length,
+            torneos: docs.map(doc =>{
+                return {
+                    _id: doc._id,
+                    creadoEl: doc.creadoEl,
+                    nombre: doc.nombre,
+                    facultad: doc.facultad,
+                    fechaEvento: doc.fechaEvento,
+                    precioEntrada: doc.precioEntrada,
+                    cupoEvento: doc.cupoEvento,
+                    lugarEvento: doc.lugar,
+                    imagenTorneo: doc.imagenTorneo,
+                    peticion:{
+                        tipo: 'GET',
+                        url: 'http://localhost:3000/torneos/'+doc._id
+                    }
+                }
+            })});
+    })
+    .catch(err => {
+        estandar.errorChecker(res,err);
+    });
 };
+exports.torneos_get_all = (req,res,next)=>{
+    
+};
+
